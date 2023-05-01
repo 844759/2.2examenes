8a19a01247d4e0b73648337592b55c47c3b98de0
Windax03 <95488416+Windax03@users.noreply.github.com>
2023-05-01 15:54:49 +0200

    Add files via upload

--- adsis/Ejercicios de shell-script/2_shell/2.3_shell.sh
+++ adsis/Ejercicios de shell-script/2_shell/2.3_shell.sh
@@ -3,3 +3,20 @@
 # modificarlos, insertando entre su primera y segunda línea el copyright del autor y la fecha, 
 # de forma que:
 
+if [ $# -ne 2 ]
+then
+    echo "Número incorrecto de parametros, introduzca un fichero"     # Se comprueba que se haya metido un fichero como parametro
+    exit 1
+fi
+
+# Obtener el contenido del fichero de licencia
+licencia=$(cat $2)
+
+while read archivo
+do
+    # Se utiliza el comando sed con el flag -i para modificar el archivo en la
+    # linea seleccionada en este caso la segunda indicada con 2i, posteriormente 
+    # se usa date para obtener la fecha y seleccionamos el archivo que se modificara
+    sed -i "2i # $licencia\n# $(date)\n" "$archivo"
+
+done < $1
