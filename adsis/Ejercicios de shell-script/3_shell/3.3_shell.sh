342235f50a02f83ee8af8dde1f4ee7554e9cbdd9
Windax03 <95488416+Windax03@users.noreply.github.com>
2023-05-02 22:31:44 +0200

    Add files via upload

--- adsis/Ejercicios de shell-script/3_shell/3.3_shell.sh
+++ adsis/Ejercicios de shell-script/3_shell/3.3_shell.sh
@@ -11,6 +11,22 @@ grep '/home/al-03-04' $dir
 
 while read linea
 do
+    user=$( echo $linea | cut -d':' -f1 )
+
+    log=$( finger $user | grep 'Never logged in' )  # Si el usuario nunca ha iniciado sesion con grep encontraremos una coincidencia
+
+    if [ $? -eq 0 ]
+    then
+
+        echo ""$user". Usuario inactivo, se borra su cuenta"
+        userdel $user
+    else
+
+        echo ""$user". Usuario activo, copiando su web /var/tmp/"$user""
+        mkdir /var/tmp/"$user"
+        wget -m http://localhost/~"$user"
+        cp "$user" /var/tmp/"$user"
+    fi
 
     
 done < <(grep '/home/al-03-04' $dir)
